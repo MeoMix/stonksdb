397d0943aa890fec9549870afcd6972331c141c8
Sean Anderson <sanderson@foreground.co>
2021-07-24 16:39:25 -0700

    Fix

--- index.ts
+++ index.ts
@@ -7,15 +7,15 @@ import CoinGecko from 'coingecko-api';
 
 const app = express();
 
-app.use((req, res, next) => {
-  cors();
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
 });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+app.use((req, res, next) => {
+  cors();
   next();
 });
 
