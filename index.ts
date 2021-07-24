266ab89c1fb9a0787563f6d1602a4b2a6138dd19
Sean Anderson <sanderson@foreground.co>
2021-07-24 16:31:48 -0700

    coooors

--- index.ts
+++ index.ts
@@ -12,6 +12,12 @@ const app = express();
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const PORT = 8000;
 app.get('/', (req,res) => res.send('Express + TypeScript Server RAWR'));
 
